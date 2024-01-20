54f7209fe90f0c15a03a787181fa70d17caef59b
suyeonnii <suyeon9288@gmail.com>
2024-01-21 02:09:47 +0900

    fix: 수정

--- backend/apps/views.py
+++ backend/apps/views.py
@@ -179,7 +179,6 @@ class ChatRoomListView(APIView):
         }
         return Response(data)
 
-
 class ChatRoomImageUploadView(APIView):
     parser_classes = [MultiPartParser]
     @swagger_auto_schema(
